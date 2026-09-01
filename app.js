9b38a57c4cd6acfd0b347f23ab135b659c4dc2c4
paribhoge123 <paribhoge@gmail.com>
2026-09-01 23:51:48 +0530

    Changed New Page

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
-
+app.use(express.static(path.join(__dirname, "public")));
 //INDEX Route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
