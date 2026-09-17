0e448163c6c163d1dd14c360dd14a5fd075ce9a8
paribhoge123 <paribhoge@gmail.com>
2026-09-17 23:53:24 +0530

    updated

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const reviews = require("./reviews");
+const { ref } = require("joi");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -8,15 +10,21 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    filename: String,
-    url: String,
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dHJhdmVsJTIwc2NlbmVyeXxlbnwwfHwwfHx8&auto=format&fit=crop&w=500&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dHJhdmVsJTIwc2NlbmVyeXxlbnwwfHwwfHx8&auto=format&fit=crop&w=500&q=60"
+        : v,
   },
   price: {
-  type: Number,
-  required: true
-},
+    type: Number,
+    required: true,
+  },
   location: String,
   country: String,
+  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
